ab38012dcdfd66eef6299e7076005d01d3488519
shwon96 <wsh4872@gmail.com>
2020-09-27 08:56:33 +0900

    2020-09-27
    2020-09-27

---
+++ Study_2020_python/Dynamic Programming/N9625.py
@@ -0,0 +1,21 @@
+# 작성자 : SH_WON_96
+# 2020-09-27
+# 알고리즘 - DP
+# 문제번호 : 9625 BABBA
+
+# 초기 입력값 받기
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10000)
+
+test = int(input())
+
+A = 1
+B = 0
+
+for i in range(test):
+    tmp = B
+    B = A + B
+    A = tmp
+
+print (str(A)+" "+str(B))
